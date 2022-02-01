aeb4757ec3211d6d792ef2e885632db48446800e
Beomjin Kim <bamjin204@gmail.com>
2022-02-01 14:49:06 +0900

    텍스트 변환

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,6 +2,7 @@
 
 // To change portfolio colors globally go to the  _globalColor.scss file
 
+import { useContext } from "react";
 import emoji from "react-easy-emoji";
 
 // Summary And Greeting Section
@@ -156,8 +157,8 @@ const bigProjects = {
     {
       image: require("./assets/images/saayaHealthLogo.webp"),
       projectName: "멀티프로세서 기반 스마트 IoT 피트니스 매트(Smart IoT Fitness Mattress based on Multi-Processor)",
-      projectDesc: `I2C통신 기반 다수의 Atmega 128 CPU 멀티프로세서 구성으로 다수의 센서정보 실시간 송수신(BT&Serial)`+
-      <br/>+`Java 기반 Guide SoftWare 탑재`
+      projectDesc: utext="I2C통신 기반 다수의 Atmega 128 CPU 멀티프로세서 구성으로 다수의 센서정보 실시간 송수신(BT&Serial)\nJava 기반 Guide SoftWare 탑재"
+    
     },
     
   ],
