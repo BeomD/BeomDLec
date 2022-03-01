84da00eb0db307ddf26a426462492a724df59afa
BeomD <bamjin_e@protonmail.ch>
2022-03-01 13:41:01 +0000

    Deploying to gh-pages from  @ 07e9e174834cf330c1e834e0436e7455fce79529 🚀

---
+++ precache-manifest.41fefa33313d89460098f75cf4528dca.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8b694e6a74a0e47c2e8f8e14736bb9",
+    "url": "/BeomDLec/index.html"
+  },
+  {
+    "revision": "2ec0a81941761267011a",
+    "url": "/BeomDLec/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "476d38f8fc1d1546692a",
+    "url": "/BeomDLec/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "5f03fcbd1a6fc5569884",
+    "url": "/BeomDLec/static/css/main.285e9f2d.chunk.css"
+  },
+  {
+    "revision": "b8eb4e6ae428418a7e92",
+    "url": "/BeomDLec/static/js/2.43d298c0.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/BeomDLec/static/js/2.43d298c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec0a81941761267011a",
+    "url": "/BeomDLec/static/js/3.b77f0e1a.chunk.js"
+  },
+  {
+    "revision": "476d38f8fc1d1546692a",
+    "url": "/BeomDLec/static/js/4.a288f67d.chunk.js"
+  },
+  {
+    "revision": "5f03fcbd1a6fc5569884",
+    "url": "/BeomDLec/static/js/main.c0d4e81d.chunk.js"
+  },
+  {
+    "revision": "f0a507c4a9607a392ba3",
+    "url": "/BeomDLec/static/js/runtime-main.22806735.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/BeomDLec/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "f7a12d9343f08f1c8bafe611f68159f3",
+    "url": "/BeomDLec/static/media/BlueM.f7a12d93.png"
+  },
+  {
+    "revision": "10d1b593092b9621d56881d768104c70",
+    "url": "/BeomDLec/static/media/Haizen.10d1b593.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/BeomDLec/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b83559aa1cb6aa9787982752e662ddef",
+    "url": "/BeomDLec/static/media/Picksu.b83559aa.png"
+  },
+  {
+    "revision": "0bd6a074ad6b137129abe56a0787de1e",
+    "url": "/BeomDLec/static/media/airbnbLogo.0bd6a074.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/BeomDLec/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "34578cf4d690600cc3cf0a26fc642b47",
+    "url": "/BeomDLec/static/media/denpic.34578cf4.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/BeomDLec/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/BeomDLec/static/media/developerActivity2.59389695.svg"
+  },
+  {
+    "revision": "0bd6a074ad6b137129abe56a0787de1e",
+    "url": "/BeomDLec/static/media/facebookLogo.0bd6a074.png"
+  },
+  {
+    "revision": "60131220fa91f60c1f6c83182eea9e32",
+    "url": "/BeomDLec/static/media/harvardLogo.60131220.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/BeomDLec/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8201cd3c6d9b3eafdf0c42c4a610bfdd",
+    "url": "/BeomDLec/static/media/saayaHealthLogo.8201cd3c.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/BeomDLec/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/BeomDLec/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "6ec2aea38afc305d05763bb521af8b30",
+    "url": "/BeomDLec/static/media/winoi.6ec2aea3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88dac0b013106c4d87e56ea68ad5c351.js"
+  "/BeomDLec/precache-manifest.41fefa33313d89460098f75cf4528dca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BeomDLec/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
