3fe2f55d7f10d51c29ec1b408c4418efdeb49347
Beomjin Kim <bamjin_e@protonmail.ch>
2022-03-01 21:58:23 +0900

    개발자 포트폴리오 추가

--- src/portfolio.js
+++ src/portfolio.js
@@ -22,7 +22,7 @@ const greeting = {
       "끊임없이 노력하는 ",
       "수학강사 김범진입니다",
   ],
-  resumeLink:"",
+  resumeLink:"https://beomd.github.io",
   //resumeLink: "https://www.dropbox.com/s/gwxtmd1cyy9l2g1/%EA%B9%80%EB%B2%94%EC%A7%84_%EA%B8%B0%EB%B3%B8%20%EC%8B%A0%EC%9E%85%20%EC%9D%B4%EB%A0%A5%EC%84%9C%20.pdf?dl=0", 
   // Set to empty to hide the button
   
